1d7d21bc6aefa34069103e3e2eede24701c738c9
Anton <digitalworldwww@gmail.com>
2019-12-14 21:14:43 +0100

    with redux

---
+++ src/ProductsData.ts
@@ -0,0 +1,81 @@
+import { async } from "q";
+import { watchFile } from "fs";
+import { resolve } from "dns";
+
+export interface IReview {
+  comment: string;
+  reviewer: string;
+}
+
+export interface IProduct {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  reviews: IReview[];
+}
+
+export const getProducts = async (): Promise<IProduct[]> => {
+  await wait(1000);
+  return products;
+  };
+
+export const getProduct = async (id: number): Promise<IProduct | null> => {
+  await wait(1000);
+  const foundProducts = products.filter(customer => customer.id === id);
+  return foundProducts.length === 0 ? null : foundProducts[0];
+};
+
+const wait = (ms: number): Promise<void> => {
+  // tslint:disable-next-line: no-shadowed-variable
+  return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+export const products: IProduct[] = [
+  {
+    description:
+      "A collection of navigational components that composedeclaratively with your app",
+    id: 1,
+    name: "React Router",
+    price: 8,
+    reviews: [
+      {
+        comment: "Excellent! This does everything I want",
+        reviewer: "Billy"
+      },
+      { comment: "The best router I've ever worked with", reviewer: "Sally" }
+    ]
+  },
+  {
+    description: "A library that helps manage state across yourapp",
+    id: 2,
+    name: "React Redux",
+    price: 12,
+    reviews: [
+      {
+        comment: "I've found this really useful in a large app I'm working on",
+        reviewer: "Billy"
+      },
+      {
+        comment: "I've found this really useful in a large app I'm working on",
+        reviewer: "Billy"
+      }
+    ]
+  },
+  {
+    description: "A library that helps you interact with a GraphQL backend",
+    id: 3,
+    name: "React Apollo",
+    price: 12,
+    reviews: [
+      {
+        comment: "I'll never work with a REST API again!",
+        reviewer: "Billy"
+      },
+      {
+        comment: "It makes working with GraphQL backends a breeze",
+        reviewer: "Sally"
+      }
+    ]
+  }
+];
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import RoutesWrap from "./Routes";
+import { Provider } from "react-redux";
+import { Store } from "redux";
+import configureStore from "./Store";
+import { IApplicationState } from "./Store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface IProps {
+  store: Store<IApplicationState>;
+}
+
+const Root: React.SFC<IProps> = props => {
+  return (
+    <Provider store={props.store}>
+      <RoutesWrap />
+    </Provider>
+  );
+};
+
+const store = configureStore();
+ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
