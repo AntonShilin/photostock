c61a46d038336a620566eed4dd6ba450142159d5
Anton <digitalworldwww@gmail.com>
2020-01-26 17:34:05 +0200

    initial commit

--- src/Header.tsx
+++ src/Header.tsx
@@ -8,25 +8,25 @@ import { FaRegUserCircle } from "react-icons/fa";
 const Header: React.SFC<RouteComponentProps> = props => {
   return (
     <Navbar bg="dark" variant="dark" expand="md" sticky="top">
-      <NavLink to="https://antonshilin.github.io/photos" className="mr-3 p-2 text-decoration-none  text-white">
+      <NavLink to="/photos" className="mr-3 p-2 text-decoration-none  text-white">
         Photo & Video Stock
       </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto text-uppercase">
           <NavLink
-            to="https://antonshilin.github.io/photos"
+            to="/photos"
             className="mr-3 p-2 text-decoration-none  text-white"
           >
             Find photos
           </NavLink>
           <NavLink
-            to="https://antonshilin.github.io/videos"
+            to="/videos"
             className="mr-3 p-2 text-decoration-none  text-white"
           >
             Find videos
           </NavLink>
-          <NavLink to="https://antonshilin.github.io/login" className="mr-3 p-2">
+          <NavLink to="/login" className="mr-3 p-2">
             <FaRegUserCircle style={{ fontSize: "1.5rem", color: "white" }} />
           </NavLink>
         </Nav>
