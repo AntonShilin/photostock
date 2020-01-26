03f4b91de77973d1ef4398b1564a64450c06783e
Anton <digitalworldwww@gmail.com>
2020-01-26 19:28:09 +0200

    bug

--- src/ProductsReducer.ts
+++ src/ProductsReducer.ts
@@ -42,10 +42,11 @@ export const productsReducer: Reducer<IProductsState, ProductsActions> = (
       if (action.keydownKey === 13 || action.keydownKey === 32) {
         if (state.searchNamePhoto !== "") {
           const currentLocation = document.location;
+          alert(currentLocation)
           document.location.replace(currentLocation+"/"+state.searchNamePhoto);
         }
       }
-      
+
       return {
         ...state,
         keyboardKey: action.keydownKey
